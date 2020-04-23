8cfd0cc7e405033d0864ef3fccad58adc6bbee03
NavyaTatikonda <inavyatatikonda@gmail.com>
2020-04-24 00:39:58 +0530

    Create Duplicatezeros.py

---
+++ Day#3/Duplicatezeros.py
@@ -0,0 +1,17 @@
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        """
+        Do not return anything, modify arr in-place instead.
+        """
+        i=0
+        while(i<len(arr)):
+            if arr[i]!=0:
+                i=i+1
+            else :
+                arr.insert(i+1,0)
+                i=i+2
+                arr.pop()
+        
+   
+            
+        
